d6d366509027b58d0b88c65323344e14c63d5aed
Corkills <sam.r.corkill@gmail.com>
2014-07-27 12:57:03 +0100

    Programming Assignment 2 Uplaod

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix - Sets up functions for caching and caches the matrix
+## cacheSolve - Returns the matrix inverse (from a chache if previously evaluated)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+     m <- NULL                # sets value of m to NULL in function so previous results are not used
+     set <- function(y) {     # defines the function to set the matrix up
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() {x}                   # Defines the function to call the value of the matrix
+     setinv <- function(solve) {m <<- solve} # Creates a function to set the value of the inverse in the parent environment
+     getinv <- function() {m}                # Creates a function to call the value of the inverse from another environment
+     list(set = set, get = get,              # Defines function names outside the function
+          setinv = setinv,
+          getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     m <- x$getinv()               # Calls the value of m from makeCacheMatrix function
+     if(!is.null(m)) {             # Checks if the value of m has been calculated previously
+          message("getting cached data")
+          return(m)                # If the value is know it is returned and the function stops here
+     }
+     data <- x$get()          # If the value has not been calculated the value of the matrix is called from the makeCacheMartix function
+     m <- solve(data,...)     # The inverse is calculated and assigned to m
+     x$setinv(m)              # The setinv function is called to cache the value of the inverse (m) for next time
+     m                        # prints m
 }
